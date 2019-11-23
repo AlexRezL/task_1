363d4a43b0118ef474cc8675684a97fe79a15f4e
Alex_Stebakov <se_tale@mail.ru>
2019-11-23 15:59:08 +0300

    added some problems to README file, thought out a situation where the size of the article is less than 200 characters

--- index.php
+++ index.php
@@ -3,16 +3,17 @@ $articleText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi e
 
 $articleLink = "./article_full.php";
 
-$articlePreview = mb_strimwidth($articleText, 0, 200, "");
+if (strlen($articleText) > 200) {
+    $articlePreview = mb_strimwidth($articleText, 0, 200, "");
+    $thirdWord = strrpos(substr($articlePreview, 0, strrpos(substr($articlePreview, 0, strrpos($articlePreview, ' ')-1), ' ')-1), ' ');
 
-$f = strrpos($articlePreview, ' ');
-$s = strrpos(substr($articlePreview, 0, $f-1), ' ');
-$t = strrpos(substr($articlePreview, 0, $s-1), ' ');
+    $lastThreeWords = substr($articlePreview, $thirdWord+1);
+    $articlePreview = substr($articlePreview, 0, $thirdWord+1);
 
-$lastThreeWords = substr($articlePreview, $t+1);
-$articlePreview = substr($articlePreview, 0, $t+1);
-
-$articlePreview = $articlePreview."<a href=\"".$articleLink."\">".$lastThreeWords.".."."</a>";
-
-echo "<p>".$articlePreview."</p>";
+    $articlePreview = $articlePreview."<a href=\"".$articleLink."\">".$lastThreeWords."..."."</a>";
+    echo "<p>".$articlePreview."</p>";
+}
+else {
+    echo "<p>".$articleText."</p>";
+}
 ?>
