ea8f2b6d30c637f77de22ba7e8b0e0ba9d47cdab
Alex_Stebakov <se_tale@mail.ru>
2019-11-23 14:28:31 +0300

    added article clipping to 200 characters and the last three words with ellipses are made by reference

--- index.php
+++ index.php
@@ -3,6 +3,16 @@ $articleText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi e
 
 $articleLink = "./article_full.php";
 
-echo "<a href=\"".$articleLink."\">link</a>";
+$articlePreview = mb_strimwidth($articleText, 0, 200, "");
 
+$f = strrpos($articlePreview, ' ');
+$s = strrpos(substr($articlePreview, 0, $f-1), ' ');
+$t = strrpos(substr($articlePreview, 0, $s-1), ' ');
+
+$lastThreeWords = substr($articlePreview, $t+1);
+$articlePreview = substr($articlePreview, 0, $t+1);
+
+$articlePreview = $articlePreview."<a href=\"".$articleLink."\">".$lastThreeWords.".."."</a>";
+
+echo "<p>".$articlePreview."</p>";
 ?>
